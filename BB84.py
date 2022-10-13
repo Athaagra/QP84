7969877bee04767bba0a5453b1f80b58eb234452
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2022-10-12 21:36:46 -0400

    Add files via upload

--- BB84.py
+++ BB84.py
@@ -462,3 +462,115 @@ print('The number of steps per episode that solved:{}'.format(np.round(np.mean(s
 plt.plot(total_episodes)
 plt.title('The simulation has been solved the environment Deep Q learning:{}'.format(solved/episodes))
 plt.show()
+
+
+import argparse
+import gym
+import numpy as np
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
+
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.affine1 = nn.Linear(4, 128)
+        self.dropout = nn.Dropout(p=0.6)
+        self.affine2 = nn.Linear(128, 2)
+
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        x = self.affine1(x)
+        x = self.dropout(x)
+        x = F.relu(x)
+        action_scores = self.affine2(x)
+        return F.softmax(action_scores, dim=1)
+
+
+policy = Policy()
+optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+eps = np.finfo(np.float32).eps.item()
+
+
+
+def select_action(state):
+    state = torch.from_numpy(state).float().unsqueeze(0)
+    probs = policy(state)
+    m = Categorical(probs)
+    action = m.sample()
+    policy.saved_log_probs.append(m.log_prob(action))
+    return action.item()
+
+
+def finish_episode():
+    R = 0
+    policy_loss = []
+    returns = []
+    for r in policy.rewards[::-1]:
+        R = r + args.gamma * R
+        returns.insert(0, R)
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / (returns.std() + eps)
+    for log_prob, R in zip(policy.saved_log_probs, returns):
+        policy_loss.append(-log_prob * R)
+    optimizer.zero_grad()
+    policy_loss = torch.cat(policy_loss).sum()
+    policy_loss.backward()
+    optimizer.step()
+    del policy.rewards[:]
+    del policy.saved_log_probs[:]
+
+
+def mainly():
+    running_reward = 100
+    for i_episode in count(1):
+        state_n,actions,data,al_coun,al_data,bob_count,bob_mail,bob_mailbox,bob_k,done,act_hist,cum_re,state_space,action_space,max_moves,al_obs,bob_data,=reset()
+        #state, _ = env.reset()
+        ep_reward = 0
+        for t in range(1, 10):  # Don't infinite loop while learning
+            print(state_n)
+            actiona = select_action(state_n[0])
+            actionb = select_action(state_n[0])
+            action = (actiona,actionb)
+            print('This is the action {}'.format(action))
+            #state, reward, done, _, _ = env.step(action)
+            stat,re,do,action_h,bob_key=step(action,act_hist,max_moves,al_coun,data,al_data,bob_count,al_obs,bob_k,bob_data,cum_re,bob_mailbox,bob_mail,done, verbose=0,)
+            if args.render:
+                #env.render()
+                print('render episode')
+            policy.rewards.append(re)
+            ep_reward += re
+            if done:
+                break
+
+        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+        finish_episode()
+        if i_episode % args.log_interval == 0:
+            #print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+             #     i_episode, ep_reward, running_reward))
+            print('Average reward: {:.2f}'.format(
+                   running_reward))
+        #if running_reward > env.spec.reward_threshold:
+        #    print("Solved! Running reward is now {} and "
+        #          "the last episode runs to {} time steps!".format(running_reward, t))
+            break
+if __name__ == '__main__':
+    mainly()
