791c236c69ee080a0591d99d0a43cc04ba7f83d5
Athanasios Agrafiotis <46803277+Athaagra@users.noreply.github.com>
2022-10-13 16:51:30 -0400

    Add files via upload

--- BB84.py
+++ BB84.py
@@ -468,7 +468,7 @@ import argparse
 import gym
 import numpy as np
 from itertools import count
-
+#import matplotlib.pyplot.plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -492,7 +492,7 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         self.affine1 = nn.Linear(4, 128)
         self.dropout = nn.Dropout(p=0.6)
-        self.affine2 = nn.Linear(128, 2)
+        self.affine2 = nn.Linear(128, 4)
 
         self.saved_log_probs = []
         self.rewards = []
@@ -514,6 +514,17 @@ eps = np.finfo(np.float32).eps.item()
 def select_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
     probs = policy(state)
+    #test_probs = probs[0].detach().numpy()
+    #nprobs = np.nan_to_num(test_probs, copy=True, nan=0.0, posinf=None, neginf=None)
+    #print('This tensor is nan {}'.format(torch.any(probs.isnan())))
+    #if torch.any(probs.isnan()) == True:
+    print(probs)
+    probs=torch.nan_to_num(probs)
+    probs=probs+0.01
+        #probs = torch.tensor([0., 0., 0., 0.], requires_grad=True)
+        #probs != probs
+    #print('Those are the probs {}'.format(probs))
+    #nprobs = torch.from_numpy(nprobs, requires_grad=True).float().unsqueeze(0)
     m = Categorical(probs)
     action = m.sample()
     policy.saved_log_probs.append(m.log_prob(action))
@@ -540,12 +551,20 @@ def finish_episode():
 
 
 def mainly():
+    import matplotlib.pyplot as plt
     running_reward = 100
-    for i_episode in count(1):
+    episodes=100
+    solved=0
+    steps_ep=[]
+    total_episodes=[]
+    for i_episode in range(0,episodes):
+        print('This is the episode number {}'.format(i_episode))
         state_n,actions,data,al_coun,al_data,bob_count,bob_mail,bob_mailbox,bob_k,done,act_hist,cum_re,state_space,action_space,max_moves,al_obs,bob_data,=reset()
         #state, _ = env.reset()
         ep_reward = 0
-        for t in range(1, 10):  # Don't infinite loop while learning
+        do=False
+        reward_episode=[]
+        while do!=True:  # Don't infinite loop while learning
             print(state_n)
             actiona = select_action(state_n[0])
             actionb = select_action(state_n[0])
@@ -553,24 +572,275 @@ def mainly():
             print('This is the action {}'.format(action))
             #state, reward, done, _, _ = env.step(action)
             stat,re,do,action_h,bob_key=step(action,act_hist,max_moves,al_coun,data,al_data,bob_count,al_obs,bob_k,bob_data,cum_re,bob_mailbox,bob_mail,done, verbose=0,)
+            print('This is the reward {}'.format(re))
+            reward_episode.append(re)
             if args.render:
                 #env.render()
                 print('render episode')
             policy.rewards.append(re)
             ep_reward += re
-            if done:
-                break
+            state_n=stat
+            #steps_ep.append(len(reward_episode))
+            #if ep_reward > 0:
+            #    solved+=1
+            if reward_episode[-1]==1:
+                solved+=1
+                steps_ep.append(len(reward_episode))
+            #policy.rewards.append(re)
+            #ep_reward += re
+            total_episodes.append(reward_episode[-1])
+
+            #if done:
+            #    break
 
         running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
         finish_episode()
-        if i_episode % args.log_interval == 0:
+        if i_episode % args.log_interval == 100:
             #print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
              #     i_episode, ep_reward, running_reward))
             print('Average reward: {:.2f}'.format(
                    running_reward))
+            break
+        else:
+            continue
         #if running_reward > env.spec.reward_threshold:
         #    print("Solved! Running reward is now {} and "
         #          "the last episode runs to {} time steps!".format(running_reward, t))
-            break
+    print('The simulation has been solved the environment PPO:{}'.format(solved/episodes))
+    print('The number of steps per episode that solved:{}'.format(np.round(np.mean(steps_ep))))
+    plt.plot(total_episodes)
+    plt.title('The simulation has been solved the environment PPO:{}'.format(solved/episodes))
+    plt.show()
+    return total_episodes
+
 if __name__ == '__main__':
-    mainly()
+    tot_ep=mainly()
+
+
+#import torch
+#import torch.nn as nn
+import numpy as np
+#import torch.optim as optim
+import gym
+import random
+import math
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import sys
+HISTORY_LENGTH = 1  
+#hyperparameters
+D = 4#len(env.reset())*HISTORY_LENGTH
+M = 20
+K = 4  
+print('This is the D {} M {} and K {}'.format(D,M,K))
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+   
+def relu(x):
+   return x *(x >0)
+    
+class ANN:
+    def __init__(self, D, M, K, f=relu):
+        self.D=D
+        self.M=M
+        self.K=K
+        self.f=f
+    def init(self):
+        D, M, K = self.D, self.M, self.K
+        self.W1 = np.random.randn(D, M) /np.sqrt(D)
+        self.b1 = np.zeros(M)
+        self.W2 = np.random.randn(M, K)/ np.sqrt(M)
+        self.b2 = np.zeros(K)
+            
+    def forward(self, X):
+        #self.f
+        #print('This is the input data X{}'.format(X[0]))
+        #print('This is the w1 {}'.format(self.W2))
+        #print('This is the b1 {}'.format(self.b2.shape))
+        Z = np.tanh(X.dot(self.W1)+ self.b1)
+        return softmax(Z.dot(self.W2)+ self.b2)
+        
+    def sample_action(self, x):
+        #X=np.atleast_2d(x)
+        X=x
+        Y=self.forward(X)
+        #print('Forward process {}'.format(P))
+        y=Y[0]
+        return np.argmax(y)
+        
+    def get_params(self):
+        return np.concatenate([self.W1.flatten(), self.b1, self.W2.flatten(), self.b2])
+        
+    def get_params_dict(self):
+        return {
+            'W1': self.W1,
+            'b1': self.b1,
+            'W2': self.W2,
+            'b2': self.b2
+            }
+    def set_params(self, params):
+        D,M,K = self.D, self.M, self.K
+        self.W1 = params[:D * M].reshape(D, M)
+        self.b1 = params[D*M:D*M +M]
+        self.W2 = params[D * M + M:D*M+M+M*K].reshape(M,K)
+        self.b2 = params[-K:]
+        
+def evolution_strategy(f,population_size, sigma, lr, initial_params, num_iters):
+    #assume initial params is a 1-D array
+    num_params = len(initial_params)
+    reward_per_iteration = np.zeros(num_iters)
+    learning_rate = np.zeros(num_iters)
+    sigma_v = np.zeros(num_iters)
+    parms = np.zeros(num_iters)
+    params = initial_params
+    for t in range(num_iters):
+        t0 = datetime.now()
+        N = np.random.randn(population_size, num_params)
+        ### slow way
+        R = np.zeros(population_size)
+        acts=np.zeros(population_size)
+        #loop through each "offspring"
+        for j in range(population_size):
+            params_try = params + sigma * N[j]
+            R[j],acts[j] = f(params_try)
+        m = R.mean()
+        s = R.std()+0.001
+        #print('This is s {}'.format(s))
+        if s == 0:
+            # we cannot apply the following equation
+            print("Skipping")
+            continue
+        
+        A = (R-m)/s
+        reward_per_iteration[t]= m
+        params = params + lr/(population_size*sigma)+np.dot(N.T, A)
+        parms[t]=params.mean() 
+        learning_rate[t]=lr
+        sigma_v[t]=sigma
+        #update the learning rate
+        #lr *= 0.001
+        lr *=0.992354
+        sigma += 0.7891
+        print("Iter:",t, "Avg Reward: %.3f" % m, "Max:", R.max(), "Duration:",(datetime.now()-t0))
+        if m > R.max()/1.5 or R.max() > 300:
+            actis = acts
+        else:
+            actis=np.zeros(population_size)
+    return params, reward_per_iteration,actis,learning_rate,sigma_v,population_size,parms
+    
+def reward_function(params):
+    model = ANN(D, M, K)
+    model.set_params(params)
+    # play one episode and return the total reward
+    episode_reward = 0
+    episode_length = 0
+    done = False
+    state_n,actions,data,al_coun,al_data,bob_count,bob_mail,bob_mailbox,bob_k,done,act_hist,cum_re,state_space,action_space,max_moves,al_obs,bob_data,=reset()
+    obs = state_n[0]#obs[0]
+    obs_dim= len(obs)
+    if HISTORY_LENGTH >1:
+        state =np.zeros(HISTORY_LENGTH*obs_dim)
+        state[obs_dim:] = obs
+    else:
+        state = obs
+    while not done:
+        #get the action
+        #print('This is the state {}'.format(state))
+        actiona = model.sample_action(state_n[0])
+        actionb = model.sample_action(state_n[0])
+        action=(actiona,actionb)
+        #print('This is the action {}'.format(action))
+        #print('This is the action {}'.format(action))
+        #perform the action
+        stat,re,do,action_h,bob_key=step(action,act_hist,max_moves,al_coun,data,al_data,bob_count,al_obs,bob_k,bob_data,cum_re,bob_mailbox,bob_mail,done, verbose=0,)
+        #update total reward
+        obs=stat
+        episode_reward += re
+        episode_length +=1
+        #update state
+        if HISTORY_LENGTH > 1:
+            state = np.roll(state, -obs_dim)
+            state[-obs_dim:]=obs
+        else:
+            state = obs
+    return episode_reward,action
+    
+if __name__=='__main__':
+    model = ANN(D,M,K)
+    if len(sys.argv) > 1 and sys.argv[1] =='play':
+        #play with a saved model
+        j = np.load('es_cartpole_results.npz')
+        best_params = np.concatenate([j['W1'].flatten(), j['b1'], j['W2'].flatten(), j['b2']])
+        # in case intial shapes are not correct
+        D, M =j['W1'].shape
+        K = len(j['b2'])
+        model.D, model.M, model.K = D, M, K
+        x=np.arange(0,len(j['train']))
+        plt.figure(figsize=(13, 13))
+        plt.plot(x, j['train'], label='Rewards')
+        plt.grid(True,which="both",ls="--",c='gray') 
+        plt.title(f"Best reward={j['train'].max()}")
+        plt.legend()
+        plt.xlabel(f'Number of Steps of episode')
+        plt.ylabel('Rewards')
+        plt.savefig("Rewards-evolutionstrategy.png")
+        plt.show()
+        x=np.arange(0,len(j['learning_rate_v']))
+        plt.figure(figsize=(13, 13))
+        plt.plot(x, j['learning_rate_v'], label='learning_rate')
+        plt.plot(x, j['sigmav'], label='sigma')
+        plt.grid(True,which="both",ls="--",c='gray') 
+        plt.title(f"Population size={j['populat_s']}")
+        plt.legend()
+        plt.xlabel(f'Number of Steps of episode')
+        plt.ylabel('Learing rate')
+        plt.savefig("learning_rate-evolutionstrategy.png")
+        plt.show()
+        x=np.arange(0,len(j['learning_rate_v']))
+        plt.figure(figsize=(13, 13))
+        plt.plot(x, j['pmeters'], label='weights')
+        plt.grid(True,which="both",ls="--",c='gray') 
+        plt.title(f"Weight Average={j['pmeters'].mean()}")
+        plt.legend()
+        plt.xlabel(f'Number of Steps of episode')
+        plt.ylabel('Weights')
+        plt.savefig("NN-evolutionstrategy.png")
+        plt.show()
+    else:
+        # train and save
+        model.init()
+        params = model.get_params()
+        best_params, rewards, actions, learn_r,sigmv,pop_s,parms = evolution_strategy(
+            f=reward_function,
+            population_size=80,
+            sigma=0.1,
+            lr=0.0003,
+            initial_params=params,
+            num_iters=450,
+        )
+            
+        model.set_params(best_params)
+        np.savez('es_cartpole_results.npz',
+                 learning_rate_v=learn_r,
+                 sigmav=sigmv,
+                 populat_s=pop_s,
+                 pmeters=parms,
+                 actions_e=actions,
+                 train=rewards,
+                 **model.get_params_dict(),
+        )
+        #play 5 test episodes
+        #env.set_display(True)
+        state_n,actions,data,al_coun,al_data,bob_count,bob_mail,bob_mailbox,bob_k,done,act_hist,cum_re,state_space,action_space,max_moves,al_obs,bob_data,=reset()
+        #env.reset()
+        for t in range(0,len(actions)):
+            render()
+            stat,re,do,action_h,bob_key=step(actions[t],act_hist,max_moves,al_coun,data,al_data,bob_count,al_obs,bob_k,bob_data,cum_re,bob_mailbox,bob_mail,done, verbose=0,)
+        for _ in range(5):
+            print("Test:", reward_function(best_params))
